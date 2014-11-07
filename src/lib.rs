b1f4ea98a1e2ad335a9df4091cb49bf545e40666
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-07 13:27:28 -0700

    Remove unnecessary features

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,6 @@
 #![license = "MIT"]
 #![doc(html_root_url = "http://www.rust-ci.org/epsilonz/monad.rs/doc/monad/")]
 
-// #![feature(overloaded_calls)]
-#![feature(unboxed_closures)]
-
 //! This crate implements various monad structures.
 
 pub mod monad;
