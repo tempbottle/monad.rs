dd994145fbbb2a6edf5e65354941ca12485a0838
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-07 11:52:33 -0700

    Move 'point', 'get', 'put' out of impl

--- src/monad/state/mod.rs
+++ src/monad/state/mod.rs
@@ -1,17 +1,10 @@
 pub struct State<'s,S,A>(proc(S):'s -> (A,S));
 
 impl <'s,S,A> State<'s,S,A>
-    where
-        S:Clone,
 {
-
     pub fn run(self, s:S) -> (A,S) {
         let State(state) = self;
         state(s)
-    }
-
-    pub fn point(a:A) -> State<'s,S,A> {
-        State(proc(s) (a,s))
     }
 
     pub fn bind<B>(self, f:proc(A) -> State<'s,S,B>) -> State<'s,S,B>
@@ -21,13 +14,19 @@ impl <'s,S,A> State<'s,S,A>
             f(a).run(t)
         })
     }
+}
 
-    pub fn get() -> State<'s,S,S> {
-        State(proc(s:S) { (s.clone(), s) })
-    }
+pub fn point<'s,S,A>(a:A) -> State<'s,S,A> {
+    State(proc(s) (a,s))
+}
 
-    pub fn put(s:S) -> State<'s,S,()> {
-        State(proc(_) { ((), s) })
-    }
+pub fn get<'s,S>() -> State<'s,S,S>
+    where
+        S:Clone,
+{
+    State(proc(s:S) { (s.clone(), s) })
+}
 
+pub fn put<'s,S>(s:S) -> State<'s,S,()> {
+    State(proc(_) { ((), s) })
 }
