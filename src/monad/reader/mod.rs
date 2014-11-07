e71f470dea740ae560c4cd5e3223569862f424b1
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-07 12:15:51 -0700

    Inline all the things

--- src/monad/reader/mod.rs
+++ src/monad/reader/mod.rs
@@ -5,15 +5,18 @@ impl <'r,E,A> Reader<'r,E,A>
         E:Clone,
 {
 
+    #[inline]
     pub fn run(self, e:E) -> A {
         let Reader(reader) = self;
         reader(e)
     }
 
+    #[inline]
     pub fn point(a:A) -> Reader<'r,E,A> {
         Reader(proc(_) { a })
     }
 
+    #[inline]
     pub fn bind<B>(self, f:proc(A) -> Reader<'r,E,B>) -> Reader<'r,E,B>
         where
             B:Clone,
@@ -21,12 +24,14 @@ impl <'r,E,A> Reader<'r,E,A>
         Reader(proc(e:E) { f(self.run(e.clone())).run(e) })
     }
 
+    #[inline]
     pub fn local(self, f:proc(E) -> E) -> Reader<'r,E,A> {
         Reader(proc(e) { self.run(f(e)) })
     }
 
 }
 
+#[inline]
 pub fn ask<'r,E>() -> Reader<'r,E,E> {
     Reader(proc(e) { e })
 }
--- src/monad/state/mod.rs
+++ src/monad/state/mod.rs
@@ -2,11 +2,13 @@ pub struct State<'s,S,A>(proc(S):'s -> (A,S));
 
 impl <'s,S,A> State<'s,S,A>
 {
+    #[inline]
     pub fn run(self, s:S) -> (A,S) {
         let State(state) = self;
         state(s)
     }
 
+    #[inline]
     pub fn bind<B>(self, f:proc(A) -> State<'s,S,B>) -> State<'s,S,B> {
         State(proc(s) {
             let (a,t) = self.run(s);
@@ -14,15 +16,18 @@ impl <'s,S,A> State<'s,S,A>
         })
     }
 
+    #[inline]
     pub fn seq<B>(self, m:State<'s,S,B>) -> State<'s,S,B> {
         self.bind(proc(_) m)
     }
 }
 
+#[inline]
 pub fn point<'s,S,A>(a:A) -> State<'s,S,A> {
     State(proc(s) (a,s))
 }
 
+#[inline]
 pub fn get<'s,S>() -> State<'s,S,S>
     where
         S:Clone,
@@ -30,6 +35,7 @@ pub fn get<'s,S>() -> State<'s,S,S>
     State(proc(s:S) { (s.clone(), s) })
 }
 
+#[inline]
 pub fn put<'s,S>(s:S) -> State<'s,S,()> {
     State(proc(_) { ((), s) })
 }
