dc2c3e33499068270bbd06e126e2d0220ad86fd5
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-14 01:57:14 -0700

    Add mdo! macro for do-notation; fix #3

---
+++ macros/src/lib.rs
@@ -0,0 +1,35 @@
+#![feature(macro_rules)]
+
+#[macro_export]
+macro_rules! mdo {
+    (
+        let $p: path = $e: expr ; $( $t: tt )*
+    ) => (
+            { let $p = $e ; mdo! { $( $t )* } }
+    );
+    (
+        let $p: path : $ty: ty = $e: expr ; $( $t: tt )*
+    ) => (
+            { let $p: $ty = $e ; mdo! { $( $t )* } }
+    );
+    (
+        $p: pat : $ty: ty <- $e: expr ; $( $t: tt )*
+    ) => {
+            bind($e, move |: $p : $ty | mdo! { $( $t )* } )
+    };
+    (
+        $p: pat <- $e: expr ; $( $t: tt )*
+    ) => {
+            bind($e, move |: $p | mdo! { $( $t )* } )
+    };
+    (
+        seq $e: expr ; $( $t: tt )*
+    ) => {
+            bind($e, move |: _ | mdo! { $( $t )* });
+    };
+    (
+        end $e: expr
+    ) => {
+            $e
+    };
+}
