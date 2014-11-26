2ac603cf4c354f025cb502ef5b6a24fecdc295a2
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-26 12:39:09 -0700

    Add docs

---
+++ doc/search-index.js
@@ -0,0 +1,11 @@
+var searchIndex = {};
+searchIndex['monad'] = {"items":[[0,"","monad","This crate implements various monad structures."],[0,"monad","",""],[0,"identity","monad::monad",""],[0,"free","monad::monad::identity",""],[1,"Sig","monad::monad::identity::free",""],[1,"Identity","",""],[3,"point","",""],[3,"bind","",""],[3,"wrap","",""],[10,"map","","",0],[10,"point","","",0],[10,"bind","","",0],[10,"seq","","",0],[10,"resume","","",0],[10,"go","","",0],[10,"run","","",0],[0,"reader","monad::monad",""],[0,"trampoline","monad::monad::reader",""],[1,"Reader","monad::monad::reader::trampoline",""],[3,"bind","",""],[3,"point","",""],[3,"ask","",""],[10,"run","","",1],[10,"bind","","",1],[10,"local","","",1],[0,"state","monad::monad",""],[0,"free","monad::monad::state",""],[1,"State","monad::monad::state::free",""],[2,"Sig","",""],[12,"Get","","",2],[12,"Put","","",2],[3,"map","",""],[3,"point","",""],[3,"bind","",""],[3,"wrap","",""],[3,"get","",""],[3,"put","",""],[10,"map","","",3],[10,"point","","",3],[10,"bind","","",3],[10,"seq","","",3],[10,"resume","","",3],[10,"go","","",3],[10,"run","","",3],[0,"trampoline","monad::monad::state",""],[1,"State","monad::monad::state::trampoline",""],[3,"bind","",""],[3,"point","",""],[3,"get","",""],[3,"put","",""],[10,"run","","",4],[10,"bind","","",4],[10,"seq","","",4]],"paths":[[1,"Identity"],[1,"Reader"],[2,"Sig"],[1,"State"],[1,"State"]]};
+searchIndex['tailrec'] = {"items":[[0,"","tailrec",""],[0,"trampoline","",""]],"paths":[]};
+
+searchIndex['free'] = {"items":[[0,"","free",""],[0,"free","",""],[0,"trampoline","free::free",""],[1,"Trampoline","free::free::trampoline",""],[3,"point","",""],[3,"bind","",""],[3,"wrap","",""],[3,"done","",""],[3,"more","",""],[4,"Sig","",""],[10,"map","","",0],[10,"point","","",0],[10,"bind","","",0],[10,"seq","","",0],[10,"resume","","",0],[10,"go","","",0],[10,"run","","",0]],"paths":[[1,"Trampoline"]]};
+
+searchIndex['monad_macros'] = {"items":[[0,"","monad_macros",""],[15,"mdo!","",""]],"paths":[]};
+
+searchIndex['free_macros'] = {"items":[[0,"","free_macros",""],[15,"monad!","",""]],"paths":[]};
+
+initSearch(searchIndex);
